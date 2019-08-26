fb9063d01106d920aeade6d2f878589075c5f4a3
sbishop <sbishop@navarik.com>
2019-08-26 11:34:23 -0700

    Find words in diagonals

--- words/src/Lib.hs
+++ words/src/Lib.hs
@@ -5,6 +5,7 @@ module Lib
     , findWords
     , findWordInLine
     , getLines
+    , skew
     -- , findWordInGrid
     ) where
 
@@ -25,7 +26,7 @@ findWord grid word =
   let found = or $ map (findWordInLine word) (getLines grid)
   in if found then Just word else Nothing
 
--- findWords :: Grid -> [String] -> [Bool]
+findWords :: Grid -> [String] -> [String]
 findWords grid words = 
   let foundWords = map (findWord grid) words
   in catMaybes foundWords
@@ -37,5 +38,15 @@ getLines :: Grid -> [String]
 getLines grid =
   let horizontal = grid
       vertical = transpose grid
-      lines = horizontal ++ vertical
+      diagonal1 = diagonalize grid
+      diagonal2 = diagonalize $ map reverse grid
+      lines = horizontal ++ vertical ++ diagonal1 ++ diagonal2
   in lines ++ (map reverse lines)
+
+diagonalize :: Grid -> Grid
+diagonalize = transpose . skew
+
+skew :: Grid -> Grid
+skew [] = []
+skew (l:ls) = l : skew (map indent ls)
+  where indent line = '_' : line 
