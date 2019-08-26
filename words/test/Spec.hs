496ae92bb9c2b83e56d6b0bb23c8724b2a6938d4
sbishop <sbishop@navarik.com>
2019-08-26 12:01:48 -0700

    Unit tests

--- words/test/Spec.hs
+++ words/test/Spec.hs
@@ -7,3 +7,20 @@ main = hspec $ do
   describe "formatGrid" $ do
     it "Should concatenate every line with a newline" $ do
       (formatGrid ["abc", "def", "ghi"]) `shouldBe` "abc\ndef\nghi\n"
+
+  describe "findWord" $ do
+    it "Should find words that exist on the Grid" $ do
+      findWord grid "HASKELL" `shouldBe` Just "HASKELL"
+      findWord grid "PERL" `shouldBe` Just "PERL"
+      
+    it "Should not find words that don't exist on the Grid" $ do
+      findWord grid "HAMSTER" `shouldBe` Nothing
+
+  describe "findWords" $ do
+    it "Should find all the words that exist on the Grid" $ do
+      findWords grid languages `shouldBe` languages
+      
+    it "Should not find words that don't exist on the Grid" $ do
+      findWords grid ["FRENCH", "GERMAN", "ENGLISH"] `shouldBe` []
+
+  
